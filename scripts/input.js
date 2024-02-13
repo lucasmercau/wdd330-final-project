3bdfac9010f0f1770af67b02a97cb75f2c156af6
lucasmercau <lucas.mercau99@gmail.com>
2024-02-13 13:02:32 -0300

    yupdate input

--- scripts/input.js
+++ scripts/input.js
@@ -7,17 +7,15 @@ const books = document.querySelector(".books");
 function processInput() {
     // Get the input value
     let inputValue = document.getElementById("titleInput").value;
-    
     // Call a function with the input value
     myFunction(inputValue);
 }
 
 async function myFunction(inputValue) {
-    // Do something with the input
+
     const list = await getBooksData(inputValue);
     const listBooks = new ProductList(list, books);
     console.log("Input value:", inputValue);
-    // You can perform further processing here
     listBooks.init();
 }
 
